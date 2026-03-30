454f10fd70604df9ba2b70595230b464c64334f3
oni-suhaas <suhaasoni@gmail.com>
2026-03-30 21:57:34 +0530

    Commit changes

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const app = express();
 // ✅ MIDDLEWARE
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static("public"));
 
 // ✅ HOME PAGE
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // ✅ MONGODB CONNECT
